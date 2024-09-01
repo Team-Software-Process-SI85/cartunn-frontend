5b89d42da7046af94d9a1fc621999ec57103a999
rivacortezf <u202215313@upc.edu.pe>
2024-09-01 15:41:21 -0500

    feat:added profile-management(bounded context) & List-Products(component)

---
+++ src/app/products/components/interaction-crud/upload/upload.component.ts
@@ -0,0 +1,38 @@
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import { NgForm } from "@angular/forms";
+import { Product} from "../../../model/product.entity";
+import { ProductsService} from "../../../services/products-api/products.service";
+
+@Component({
+  selector: 'app-upload',
+  templateUrl: './upload.component.html',
+  styleUrl: './upload.component.css'
+})
+export class UploadComponent {
+  @ViewChild('fileInput') fileInput!: ElementRef;
+  product: Product = new Product();
+
+  constructor(private productService: ProductsService) {
+  }
+
+  onUploadClick(): void {
+    this.fileInput.nativeElement.click();
+  }
+
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      const file = input.files[0];
+      this.product.image = URL.createObjectURL(file);
+    }
+  }
+
+  submitNotification(form: NgForm): void {
+    if (form.valid) {
+      this.productService.uploadProduct(this.product).subscribe(() => {
+        console.log('Product uploaded successfully');
+        form.reset();
+      });
+    }
+  }
+}
