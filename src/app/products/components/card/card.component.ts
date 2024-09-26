a427e853822c7c4bd0589fbee8ea5b95118d4b48
rivacortezf <u202215313@upc.edu.pe>
2024-09-26 18:58:27 -0500

    fix: change implemented price

--- src/app/products/components/card/card.component.ts
+++ src/app/products/components/card/card.component.ts
@@ -1,22 +1,29 @@
-import { Component } from '@angular/core';
-import {ProductsService} from "../../services/products-api/products.service";
-import {Product} from "../../model/product.entity";
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from "../../services/products-api/products.service";
+import { Product } from "../../model/product.entity";
 
 @Component({
   selector: 'app-card-products',
   templateUrl: './card.component.html',
-  styleUrl: './card.component.css'
+  styleUrls: ['./card.component.css']
 })
-export class ProductsCardComponent {
-  title:string = 'Products';
+export class ProductsCardComponent implements OnInit {
+  title: string = 'Products';
   products: Product[] = [];
+  total: number = 0;
 
-  constructor(private productApiService: ProductsService) {
-  }
-  ngOnInit(): void{
+  constructor(private productApiService: ProductsService) { }
+
+  ngOnInit(): void {
     this.productApiService.getProducts()
-      .subscribe((data: any) => {
-      this.products = data;
-    });
+      .subscribe((data: Product[]) => {
+        this.products = data;
+        this.calculateTotal();
+      });
+  }
+
+
+  calculateTotal(): void {
+    this.total = this.products.reduce((sum, product) => sum + product.price, 0);
   }
 }
